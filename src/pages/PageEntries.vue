5e891fa7823099591551b49cf7cc841335f7d837
abbdanlin22 <daniel.lindvall@abbgymnasiet.se>
2025-04-01 10:34:39 +0200

    new changes to UI, Obs still some buggs and errors

--- src/pages/PageEntries.vue
+++ src/pages/PageEntries.vue
@@ -2,15 +2,24 @@
   <q-page class="">
     <div class="q-pa-md" style="">
       <q-list bordered separator>
-        <q-item v-for="entry in entries" :key="entry.id">
-          <q-item-section class="text-weight-bold" :class="useAmountColorClass(entry.amount)">
-            {{ entry.name }}
-          </q-item-section>
+        <q-slide-item v-for="entry in entries" :key="entry.id" @left="onLeft" @right="onEntrySlideRight left - color="
+          positive" right-color="negative">
+          <!-- <template v-slot:left>
+            <q-icon name="done" />
+          </template> -->
+          <template v-slot:right>
+            <q-icon name="delete" />
+          </template>
+          <q-item>
+            <q-item-section class="text-weight-bold" :class="useAmountColorClass(entry.amount)">
+              {{ entry.name }}
+            </q-item-section>
 
-          <q-item-section side class="text-weight-bold" :class="useAmountColorClass(entry.amount)">
-            {{ useCurrencify(entry.amount) }}
-          </q-item-section>
-        </q-item>
+            <q-item-section side class="text-weight-bold" :class="useAmountColorClass(entry.amount)">
+              {{ useCurrencify(entry.amount) }}
+            </q-item-section>
+          </q-item>
+        </q-slide-item>
       </q-list>
     </div>
     <q-footer class="bg-transparent">
@@ -19,21 +28,21 @@
           Balance
         </div>
         <div :class="useAmountColorClass(balance)" class="col text-h6 text-right">
-          {{ useCurrencity(balance) }}
+          {{ useCurrencify(balance) }}
         </div>
       </div>
-      <div class="row q-px-sm q-pb-sm q-col-gutter-sm bg-foter">
+      <q-form @submit="addEntry" class="row q-px-sm q-pb-sm q-col-gutter-sm bg-foter">
         <div class="col">
-          <q-input placeholder="Name" bg-color="white" outlined dense />
+          <q-input v-model="addEntryForm.name" ref="nameRef" placeholder="Name" bg-color="white" outlined dense />
         </div>
         <div class="col">
-          <q-input input-class="text-right" placeholders="Amount" bg-color="white" type="number" step="0.01" outlined
-            dense />
+          <q-input v-model="addEntryForm.amount" input-class="text-right" placeholders="Amount" bg-color="white"
+            type="number" step="0.01" outlined dense />
         </div>
         <div class="col col-auto">
-          <q-btn color="primary" icon="add" round />
+          <q-btn color="primary" icon="add" type="submit" round />
         </div>
-      </div>
+      </q-form>
     </q-footer>
   </q-page>
 </template>
@@ -60,9 +69,10 @@ entries
 */
 
 // import { fasZ } from '@quasar/extras/fontawesome-v6'
-import { ref, computed } from 'vue'
+import { ref, computed, reactive } from 'vue'
 import { useCurrencify } from 'src/use/useCurrencify'
 import { useAmountColorClass } from 'src/use/useAmountColorClass'
+import { uid } from 'quasar'
 
 const entries = ref([
   {
@@ -89,10 +99,35 @@ const entries = ref([
 
 const balance = computed(() => {
   return entries.value.reduce((accumulator, { amount }) => {
-    return accumulator + amount
-  }, 0)
+    return accumulator + (Number(amount) || 0);
+  }, 0);
+});
+
+const nameRef = ref(null)
+
+const addEntryForm = reactive({
+  name: '',
+  amount: null
 })
 
+const addEntryFormReset = () => {
+  addEntryForm.name = ''
+  addEntryForm.amount = null
+  nameRef.value.focus()
+}
+
+const addEntry = () => {
+  const newEntry = {
+    id: uid(),
+    name: addEntryForm.name,
+    amount: addEntryForm.amount
+  }
+  entries.value.push(newEntry)
+  addEntryFormReset()
+}
+const onEntrySlideRight = () => {
+  console.log('right')
+}
 // function currencyFormat(amount) {
 //   if (typeof amount !== 'number') return '';
 
